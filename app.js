357462c27a5f7951b9976a8a562a8068bb14193c
AgustinOzan <agustinozan456@gmail.com>
2025-01-29 16:35:47 -0300

    Se agrego función recorreListaAmigos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let amigos = [];
 
-//Función para agregar Amigo a la lista
+//Agregar Amigo a la lista
 function agregarAmigo(){
     
     let amigo = document.getElementById('amigo').value;
@@ -11,6 +11,24 @@ function agregarAmigo(){
         amigos.push(amigo);
         console.log(amigos);
         document.getElementById('amigo').value="";
+        recorreListaAmigos();
     }
 }
 
+
+//Recorrer lista Amigos
+function recorreListaAmigos() {
+    let elementoHTML = document.querySelector('#listaAmigos');
+
+    let fragmento = document.createDocumentFragment(); 
+
+    elementoHTML.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        fragmento.appendChild(li);
+    }
+
+    elementoHTML.replaceChildren(fragmento);
+}
